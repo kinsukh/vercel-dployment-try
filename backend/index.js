9b29715e42b5607480ab2bfda8a59083afa651e9
Kinsukh S. Sonbawne <95238196+kinsukh@users.noreply.github.com>
2023-11-21 15:31:48 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,77 +1,35 @@
-// const express = require("express")
-// const mongoose = require('mongoose')
-// const cors = require("cors")
+const express = require("express")
+const mongoose = require('mongoose')
+const cors = require("cors")
 
-// const userRoutes = require('./userRoutes/userRoutes')
-// const playlistRoutes = require('./playlistRoutes/playlistRoutes')
-
-
-// const app = express()
-// app.use(express.json())
-// app.use(cors())
-
-// mongoose.set("strictQuery", true);
-// mongoose.connect("mongodb+srv://arpitbaheti:santoshBaheti02@appu.u6vqlco.mongodb.net/VibesVault")
-
-// const db = mongoose.connection;
-// db.on("open", ()=>{
-//   console.log("database connected");
-// });
-// db.on("error", ()=>{
-//   console.log("error in connectiong to database");
-// });
-
-// app.use("/users", userRoutes);
-// app.use("/playlists", playlistRoutes);
-
-// app.use("/",(req,res) => {
-//   res.send("server is running");
-// })
-
-// const port=5000;
-
-// app.listen(port, () => {
-//   console.log("server is running")
-// })
+const userRoutes = require('./userRoutes/userRoutes')
+const playlistRoutes = require('./playlistRoutes/playlistRoutes')
 
 
-const express = require("express");
-const mongoose = require('mongoose');
-const cors = require("cors");
-
-const userRoutes = require('./userRoutes/userRoutes');
-const playlistRoutes = require('./playlistRoutes/playlistRoutes');
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
 mongoose.set("strictQuery", true);
-
-// Uncomment these lines for MongoDB connection debugging
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://arpitbaheti:santoshBaheti02@appu.u6vqlco.mongodb.net/VibesVault";
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://arpitbaheti:santoshBaheti02@appu.u6vqlco.mongodb.net/VibesVault")
 
 const db = mongoose.connection;
-
-db.on("open", () => {
-  console.log("Database connected");
+db.on("open", ()=>{
+  console.log("database connected");
 });
-
-db.on("error", (error) => {
-  console.error("Error in connecting to database:", error);
+db.on("error", ()=>{
+  console.log("error in connectiong to database");
 });
 
 app.use("/users", userRoutes);
 app.use("/playlists", playlistRoutes);
 
-app.use("/", (req, res) => {
-  res.send("Server is running");
-});
+app.use("/",(req,res) => {
+  res.send("server is running");
+})
 
-const port = process.env.PORT || 5000;
+const port=5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log("server is running")
+})
